b821fb47749933a5b499b00fd85df40db5a890d8
kien nguyen <kiennguyen@hitek.com.vn>
2024-09-19 16:43:04 +0700

    UPDATE: init src

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,21 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        dark_1: "var(--dark-1)",
+        dark_2: "var(--dark-2)",
+        dark_3: "var(--dark-3)",
+        dark_4: "var(--dark-4)",
+        light_1: "var(--light-1)",
+        light_2: "var(--light-2)",
+        light_3: "var(--light-3)",
+        light_4: "var(--light-4)",
+        gradient: "var(--gradient)",
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("daisyui")],
 };
 export default config;
